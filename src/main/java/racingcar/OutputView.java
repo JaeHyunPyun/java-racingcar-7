054ecb96e64395df288d75a5893d7de3ce48e214
jaehyunpyun <pyunjaehyun@gmail.com>
2026-04-10 17:56:49 +0900

    feat: 최종 우승자 목록 출력 기능 추가

--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -12,4 +12,14 @@ public class OutputView {
         System.out.println();
     }
 
+    public void printWinners(List<Car> winners) {
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < winners.size(); i++) {
+            System.out.print(winners.get(i).getName());
+            if (i != winners.size() - 1) {
+                System.out.print(",");
+            }
+        }
+    }
+
 }
